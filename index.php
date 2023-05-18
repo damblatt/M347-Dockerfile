d81855a0528a8c4b566d8862d6245c47ae94342a
damblatt <67500819+damblatt@users.noreply.github.com>
2023-05-18 14:46:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html >
+<html >
+<head >
+<title >M347</title >
+<meta charset ="utf-8" />
+</head >
+<body >
+<h1>M347 - Webpage mit Dockefile</h1>
+<?php
+$vorname = "Damian";
+$nachname = "Blatt";
+echo "Autor: ${vorname} ${nachname}";
+?> 
+</body >
+</html >
